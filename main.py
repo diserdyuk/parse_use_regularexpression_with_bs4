1d8b95dbca32a35192711da59f7a36959a7467b1
Denis Serdyuk <diserdyuk@gmail.com>
2020-11-02 15:23:23 +0200

    Filter and write tag

--- main.py
+++ main.py
@@ -17,21 +17,36 @@ import csv
 # .find_previous_sibling()
 
 
+def get_specialist(t):    # func.for filter text
+    whois = t.find('div', id='whois').text.strip()
+    if 'Copywriter' in whois:
+        return t
+    return None
+
+
 def main():    # hub all functions
     file = open('index.html').read()    # open file for read html code
 
     soup = BeautifulSoup(file, 'lxml')    # variable parser 
 
-    row = soup.find_all('div', class_='row')    # parse tag div, var1
-    row2 = soup.find_all('div', {'class': 'row'})    # parse tag div, var2
-    row2_1 = soup.find_all('div', {'data-set': 'salary'})
+    # row = soup.find_all('div', class_='row')    # parse tag div, var1
+    # row2 = soup.find_all('div', {'class': 'row'})    # parse tag div, var2
+    # row2_1 = soup.find_all('div', {'data-set': 'salary'})
     
-    person = soup.find('div', text='Alena').parent
-    person2 = soup.find('div', text='Alena').find_parent(class_='row')
+    # person = soup.find('div', text='Alena').parent
+    # person2 = soup.find('div', text='Alena').find_parent(class_='row')
 
 
+    # filter for tags
+    copywriters = soup.find_all('div', class_='row')    # parse all div's with class row      
 
-    print(person2)
+    copywriter = []    # list for write copywriters
+    for i in copywriters:
+        copywr = get_specialist(i)
+        if copywr:
+            copywriter.append(copywr)
+
+    print(copywriter)   
 
 
 
