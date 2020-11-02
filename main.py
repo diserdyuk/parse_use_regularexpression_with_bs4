ee344dfc79aafe1921fc01bedcc898c80faa2db7
Denis Serdyuk <diserdyuk@gmail.com>
2020-11-02 14:41:03 +0200

    Add 6 mthods bs4

--- main.py
+++ main.py
@@ -2,14 +2,39 @@ from bs4 import BeautifulSoup
 import csv
 
 
+# methods BeautifulSoup
 
-# def get_data(html):
-#     soup = BeautifulSoup(html, 'lxml')
+# .find()    # search from beginning to end
+# .find_all()
+
+# .parent     # search from end to beginning
+# .find_parent()
+
+# .parents    # analog find_all
+# .find_parents()
+
+# .find_next_sibling()    # search in an adjacent block
+# .find_previous_sibling()
 
 
 def main():    # hub all functions
     file = open('index.html').read()    # open file for read html code
-    print(file) 
+
+    soup = BeautifulSoup(file, 'lxml')    # variable parser 
+
+    row = soup.find_all('div', class_='row')    # parse tag div, var1
+    row2 = soup.find_all('div', {'class': 'row'})    # parse tag div, var2
+    row2_1 = soup.find_all('div', {'data-set': 'salary'})
+    
+    person = soup.find('div', text='Alena').parent
+    person2 = soup.find('div', text='Alena').find_parent(class_='row')
+
+
+
+    print(person2)
+
+
+
 
 
 if __name__ == '__main__':    # point of enter
